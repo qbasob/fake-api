caae692da3ef69f6b1a9267f2e0211e663395de0
Jakub Sobel <qbasob@gmail.com>
2018-07-17 10:05:17 +0200

    dodanie pola do produktów; w generatorze danych dodają się też dane userów;

--- generate-data.js
+++ generate-data.js
@@ -1,16 +1,33 @@
 const faker = require('faker');
 const fs = require('fs');
 
-let database = { products: [] };
+let database = { users: [], products: [] };
 faker.locale = 'pl';
 faker.seed(5542742864); // żeby zawsze zwracał te same rezultaty
+
+database.users = [
+    {
+        "id": 1,
+        "email": "test@gmail.com",
+        "password": "test"
+    },
+    {
+        "id": 2,
+        "email": "test2@gmail.com",
+        "password": "test2"
+    }
+];
 
 for (let i = 1; i <= 1000; i++) {
     database.products.push({
         id: i,
         name: faker.commerce.productName(),
         cost: ~~(faker.random.number() / 100),
-        quantity: ~~(faker.random.number() / 10)
+        quantity: ~~(faker.random.number() / 10),
+        points: faker.random.number({
+            min: 1,
+            max: 5
+        }),
     });
 }
 
